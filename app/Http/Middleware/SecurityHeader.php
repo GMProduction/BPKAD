40849fa1a6a2d699a3c4969b78320e46686d5f75
pradanna <pradanamahendra@gmail.com>
2024-02-21 13:40:02 +0700

    f

--- app/Http/Middleware/SecurityHeader.php
+++ app/Http/Middleware/SecurityHeader.php
@@ -15,15 +15,15 @@ class SecurityHeader extends Middleware
      *
      * @return mixed
      */
-    public function handle($request,\Closure $next, ...$guard)
+    public function handle($request, \Closure $next, ...$guard)
     {
         $response = $next($request);
         $response->headers->set('Content-Security-Policy', "block-all-mixed-content; script-src 'self' http://code.jquery.com/ https://fonts.googleapis.com/ https://fonts.gstatic.com https://cdn.datatables.net/ https://cdn.jsdelivr.net/; object-src 'self';");
-        $response->headers->set('X-Frame-Options','SAMEORIGIN');
-        $response->headers->set('X-Content-Type-Options','nosniff');
-        $response->headers->set('Referrer-Policy','same-origin');
-        $response->headers->set('Permissions-Policy','fullscreen=()');
-        $response->headers->set('Strict-Transport-Security','max-age=63072000; includeSubDomains; preload');
+        $response->headers->set('X-Frame-Options', 'SAMEORIGIN');
+        $response->headers->set('X-Content-Type-Options', 'nosniff');
+        $response->headers->set('Referrer-Policy', 'same-origin');
+        $response->headers->set('Permissions-Policy', 'fullscreen=()');
+        $response->headers->set('Strict-Transport-Security', 'max-age=63072000; includeSubDomains; preload');
         return $response;
     }
 }
