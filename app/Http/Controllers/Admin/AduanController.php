8489e05b6d76bd5d555252aa2348b7c5c411b59d
bagusyanuar <50134851+bagusyanuar@users.noreply.github.com>
2024-05-17 15:03:27 +0700

    table complaint

--- app/Http/Controllers/Admin/AduanController.php
+++ app/Http/Controllers/Admin/AduanController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Helper\CustomController;
 use App\Http\Controllers\Controller;
+use App\Models\Complaint;
 use App\Models\PublicService;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
@@ -13,8 +14,28 @@ class AduanController extends CustomController
 {
     public function index()
     {
-        return view("admin.customize.customize_aduan");
+        $data = Complaint::with([])
+            ->orderBy('year', 'DESC')
+            ->get();
+        if ($this->request->method() === 'POST') {
+            return $this->store();
+        }
+        return view("admin.customize.customize_aduan")->with(['data' => $data]);
     }
+
+    private function store()
+    {
+        try {
+            $data_request = [
+                'year' => $this->postField('year')
+            ];
+            Complaint::create($data_request);
+            return redirect()->back()->with('success', 'Berhasil menyimpan data...');
+        } catch (\Exception $e) {
+            return redirect()->back()->with('failed', 'internal server error...');
+        }
+    }
+
     public function getData()
     {
     }
---
+++ database/migrations/2024_05_17_134057_create_complaints.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('complaints', function (Blueprint $table) {
+            $table->id();
+            $table->year('year');
+            $table->text('quarter_1')->default(null)->nullable();
+            $table->text('quarter_2')->default(null)->nullable();
+            $table->text('quarter_3')->default(null)->nullable();
+            $table->text('quarter_4')->default(null)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('complaints');
+    }
+};
