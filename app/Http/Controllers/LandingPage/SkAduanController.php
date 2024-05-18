7b390b268d057e2180a3355042b5124fd267320b
bagusyanuar <bagus.yanuar613@gmail.com>
2024-05-18 12:59:30 +0700

    complaint on client

--- app/Http/Controllers/LandingPage/SkAduanController.php
+++ app/Http/Controllers/LandingPage/SkAduanController.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers\LandingPage;
 
 use App\Http\Controllers\Controller;
+use App\Models\Complaint;
 use App\Models\PublicService;
 
 class SkAduanController extends Controller
 {
     public function index()
     {
-        $service = PublicService::orderBy('year', 'DESC')->get();
-        return view('skaduan', ['data' => $service]);
+        $complaints = Complaint::with([])->orderBy('year', 'DESC')->get();
+        return view('skaduan', ['data' => $complaints]);
     }
 }
